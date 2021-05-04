4953c673549e24a00ec2ee8736bca9a22ed0f17f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-04 14:40:53 -0500

    spin up server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (_req, res) => {
+  res
+    .status(400)
+    .send(
+      "lol bad request \n \n Ahem, \n \n Cannot locate specified resource, please check your url"
+    );
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// require your server and launch it here
+const server = require("./api/server");
+
+const port = 8888;
+
+server.listen(port, () => console.log(`Server running on ${port}`));
