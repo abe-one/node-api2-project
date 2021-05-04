ed50791d4be781b09253d82944364f9f22d5c87e
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-04 16:00:23 -0500

    rewrite error messages to match readme

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,3 +1,4 @@
+const { route } = require("../server");
 const Post = require("./posts-model");
 const router = require("express").Router();
 
@@ -7,18 +8,34 @@ router.get("/", (_req, res) => {
       res.status(200).json(posts);
     })
     .catch((err) => {
-      res.status(500).json(err.message);
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
     });
 });
 
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
     .then((post) => {
-      res.status(200).json(post);
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      } else {
+        res.status(200).json(post);
+      }
     })
     .catch((err) => {
-      res.status(500).json(err.message);
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
     });
 });
+
+// route.post("/", (req, res) => {
+//   Post.insert(req.body);
+// });
 
 module.exports = router;
