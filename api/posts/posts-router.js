426fa906e395b9f7435799a92e47178512939eb8
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-04 15:25:26 -0500

    get posts endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
-// implement your posts router here
+const Post = require("./posts-model");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json(err.message);
+    });
+});
+
+module.exports = router;
