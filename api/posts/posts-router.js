5c3360f2399e58d12cc2fb6fd5037303ca440ace
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-04 15:36:14 -0500

    get postById endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,20 @@
 const Post = require("./posts-model");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_req, res) => {
   Post.find()
     .then((posts) => {
       res.status(200).json(posts);
+    })
+    .catch((err) => {
+      res.status(500).json(err.message);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      res.status(200).json(post);
     })
     .catch((err) => {
       res.status(500).json(err.message);
